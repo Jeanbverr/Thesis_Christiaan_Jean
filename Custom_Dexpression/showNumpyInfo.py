6c3d9ce890ed1d340da1f0000953bada1884990a
christiaan vanbergen <christiaan vanbergen>
2018-03-26 15:30:26 +0200

    added showNumpyInfo.py and updating model_dexpression2 to a working basic version

---
+++ Custom_Dexpression/showNumpyInfo.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+def showInfo(Var,VarName = "UnKnown"):
+	print("Name ", VarName)
+	print("type: ", type(Var))
+	print("Dtype: ", Var.dtype)
+	print("shape: ", Var.shape)
+	print("flags: ", Var.flags)
+    
+    
+
