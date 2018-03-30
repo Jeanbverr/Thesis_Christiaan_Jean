32e09f06794358a0c221ac290ad2df2e2cc848a6
christiaan vanbergen <christiaan vanbergen>
2018-03-30 14:17:59 +0200

    added things to my_dexpresion.ipynb to make it easier to transport it to condor

---
+++ Custom_Dexpression/generate_data.py
@@ -0,0 +1,5 @@
+from test_recursive_image_load_V2 import create_formated_data
+
+dataPath = 'G:/Documenten/personal/school/MaNaMA_AI/thesis/databases/wikipedia_list/cohn-Kanade/CK+'
+
+create_formated_data(dataPath)
