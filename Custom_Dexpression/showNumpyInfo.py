f309a1fbcd74b6de5f15981e23d6580bd7c2ee70
christiaan <christiaan_vanbergen@hotmail.com>
2018-05-08 15:24:04 +0200

    changed the data loading for MMI and MUG so that it works for other computers and it is more compatible with linux. seperated a few features from include_data... such as the MUG processing and the data evaluation

--- Custom_Dexpression/showNumpyInfo.py
+++ Custom_Dexpression/showNumpyInfo.py
@@ -7,11 +7,16 @@
 import numpy as np
 
 def showInfo(Var,VarName = "UnKnown"):
-	print("Name ", VarName)
-	print("type: ", type(Var))
-	print("Dtype: ", Var.dtype)
-	print("shape: ", Var.shape)
-	# print("flags: ", Var.flags)
-    
-    
+    print("Name ", VarName)
+    print("type: ", type(Var))
+    if(type(Var)==list):
+        if(len(Var) > 0):
+            print("Element type: ", type(Var[0]))
+        else:
+            print("Element type: None",)
+        print("shape: ", len(Var))
+    else:    
+        print("Dtype: ", Var.dtype)
+        print("shape: ", Var.shape)
+        # print("flags: ", Var.flags)
 
